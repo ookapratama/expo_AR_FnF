610e5392c4f57351a4d0ef1f5a82c1a1e3c03900
ookapratama <ookapratama2@gmail.com>
2024-05-12 06:39:49 +0800

    add data dummy fauna (proses)

--- datas/fauna.ts
+++ datas/fauna.ts
@@ -5,3 +5,44 @@ type faunaProps = {
   suara: undefined;
   deskripsi: string;
 };
+
+export const dataFauna: faunaProps[] = [
+  {
+    nama: "Komodo",
+    jenis: "Fauna",
+    src: require("../assets/object/fauna/komodo.png"),
+    suara: undefined,
+    deskripsi:
+      "Komodo atau Varanus Komodoensis adalah salah satu hewan langka di Indonesia. Komodo sering disebut sebagai reptil terbesar di dunia yang dapat tumbuh hingga 3 meter dan berat sekitar 70 kg atau lebih. Ciri khas mereka memiliki tubuh dan kepala yang besar, ekor yang panjang, kulit bersisik, dan cakar yang tajam. Warna kulit yang dimiliki bervariasi, dari coklat hingga abu-abu dengan bercak hitam. Saat ini habitat komodo hanya fokus pada area Pulau Komodo, Flores, Nusa Tenggara Timur (NTT) dan daerah sekitarnya. Salah satu penyebab populasi komodo berkurang adalah ulah manusia sendiri yang merusak habitat mereka dan perburuan ilegal untuk diperdagangkan di pasar internasional. Saat ini BTNK (Balai Taman Nasional Komodo) menyebut bahwa populasi komodo yang tersisa di Indonesia sebanyak 3.033 ekor (data tahun 2021). Mereka juga menerangkan bahwa penurunan populasi komodo terjadi secara alami mengikuti ketersediaan populasi mangsa atau makanan di Taman Nasional Komodo.",
+  },
+  {
+    nama: "Kucing Merah Kalimantan",
+    jenis: "Fauna",
+    src: require("../assets/object/fauna/kucing_kalimantan.png"),
+    suara: undefined,
+    deskripsi:
+      "Kucing Merah Kalimantan atau Prionailurus rubiginosus rabori adalah salah satu subspesies kucing hutan yang ada di Pulau Kalimantan. Kucing ini masuk dalam kategori hewan yang terancam punah oleh IUCN. Menurut Cat Specialist Group, kucing merah Kalimantan memiliki sedikit catatan sejarah, sehingga perkiraan kepadatan populasi mereka tidak tersedia. Organisasi tersebut memperkirakan populasi kucing Kalimantan saat ini kurang dari 2.200 ekor, dengan asumsi kepadatan 1 individu per 100 km².Kucing merah Kalimantan memiliki tubuh yang lebih besar daripada kucing domestik biasa. Mereka memiliki bulu berwarna merah batu bata dan habitat utama di hutan tropis dengan ketinggian sekitar 800 meter. Ukuran tubuh mereka berkisar antara 49,5 hingga 67 sentimeter, dengan ekor panjang sekitar 30 hingga 40,3 cm.Kucing ini sangat aktif pada malam hari, berburu pakan seperti burung kecil, tikus hutan, dan hewan kecil lainnya. Meskipun demikian, penelitian lebih lanjut sulit dilakukan karena sulitnya menemukan spesies ini di habitat alaminya.",
+  },
+  {
+    nama: "Burung Cendrawasih",
+    jenis: "Fauna",
+    src: require("../assets/object/fauna/cendrawasih.png"),
+    suara: undefined,
+    deskripsi:
+      "Burung Cenderawasih termasuk anggota keluarga Paradisaeidae dan ordo Passeriformes yang hanya dapat ditemukan di Pulau Papua, Kepulauan Maluku, Kepulauan Selat Torres, Papua Nugini hingga Australia bagian timur.Burung Cenderawasih terdiri dari beberapa genus dan spesies, yaitu 14 genus dan 43 spesies.Spesies Cenderawasih yang masih bisa ditemukan di Indonesia ada 30, 28 spesies berasal dari Papua dan 2 spesies lainnya terdapat di Kepulauan Maluku dan Halmahera. Ciri khas burung ini memiliki bulu yang panjang dan pola rumut yang tumbuh di bagian sayap, kepala, dan paruh. Burung yang mendapat julukan sebagai “Bird of Paradise” ini sempat terkenal di tahun 1522 karena bulunya yang sangat indah.Sampai abad ke-19, burung ini masih diburu oleh banyak orang untuk diambil bulunya. Seiring berjalannya waktu, masyarakat mulai sadar bahwa populasi Burung Cenderawasih semakin berkurang dan harus dilindungi kelestariannya. Tidak diketahui secara pasti populasi Burung Cenderawasih. Namun dari hasil analisisi dugaan, ada sebanyak 28 ekor/ha yang masih hidup di Papua.",
+  },
+  {
+    nama: "Komodo",
+    jenis: "Fauna",
+    src: require("../assets/object/fauna/komodo.png"),
+    suara: undefined,
+    deskripsi: "",
+  },
+  {
+    nama: "Komodo",
+    jenis: "Fauna",
+    src: require("../assets/object/fauna/komodo.png"),
+    suara: undefined,
+    deskripsi: "",
+  },
+];
